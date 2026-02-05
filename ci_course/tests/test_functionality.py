7a0d08b35e78340c7f5ec3804e274d013dbd8a70
Tom Kettlety <tom.kettlety@earth.ox.ac.uk>
2026-02-05 16:42:55 +0000

    Added to test_minimum to complete coverage

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -16,3 +16,5 @@ def test_minimum():
     assert ci_course.minimum(1, 2, 3) == 1
     assert ci_course.minimum(1.2, 2.3) == 1.2
     assert ci_course.minimum(-1.2, -3) == -3
+    assert ci_course.minimum(1, "b", "c") == 1
+    assert ci_course.minimum("1", "b", "c") == None
