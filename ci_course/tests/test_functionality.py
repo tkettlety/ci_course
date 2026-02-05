2aa83f9e30859a0b71f20bc9654c957736ec7ba5
Tom Kettlety <tom.kettlety@earth.ox.ac.uk>
2026-02-05 16:46:00 +0000

    Fixed test_minimum None bug

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -17,4 +17,4 @@ def test_minimum():
     assert ci_course.minimum(1.2, 2.3) == 1.2
     assert ci_course.minimum(-1.2, -3) == -3
     assert ci_course.minimum(1, "b", "c") == 1
-    assert ci_course.minimum("1", "b", "c") == None
+    assert ci_course.minimum("1", "b", "c") is None
